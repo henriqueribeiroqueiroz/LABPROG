a3b8117fd5fcc599b38791128cdc73f2173474a7
Rick <Rick@Rick-PC>
2017-12-25 21:31:14 -0300

    developed

--- Lab8/src/NaMosca.java
+++ Lab8/src/NaMosca.java
@@ -2,7 +2,7 @@ public interface NaMosca {
 	public String getDescricao();
 
 	public double getPreco();
-
+	
 	public void setDescricao(String descricao) throws Exception;
 
 	public void setPreco(double preco) throws Exception;
